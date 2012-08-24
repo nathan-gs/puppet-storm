4e58ff33633765efadc3afe173be11471c0b748a
Nathan Bijnens <nathan@nathan.gs>
2012-08-24 10:35:12 +0200

    storm: changed download url's because of github file download failures

--- manifests/redhat.pp
+++ manifests/redhat.pp
@@ -5,20 +5,23 @@ class storm::redhat
     package { 'zeromq':
     	ensure		=> installed,
     	provider	=> rpm,
-    	source		=> 'https://github.com/downloads/rfliam/storm-rhel-packaging/zeromq-2.2.0-1.el6.x86_64.rpm',
+    	#source		=> 'https://github.com/downloads/rfliam/storm-rhel-packaging/zeromq-2.2.0-1.el6.x86_64.rpm',
+    	source		=> 'http://dl.dropbox.com/u/25821613/storm/zeromq-2.2.0-1.el6.x86_64.rpm',
     }
 
     package { 'jzmq':
     	ensure		=> installed,
     	provider	=> rpm,
-    	source		=> "https://github.com/downloads/rfliam/storm-rhel-packaging/jzmq-2.1.0-1.el6.x86_64.rpm",
+    	#source		=> "https://github.com/downloads/rfliam/storm-rhel-packaging/jzmq-2.1.0-1.el6.x86_64.rpm",
+    	source		=> "http://dl.dropbox.com/u/25821613/storm/jzmq-2.1.0-1.el6.x86_64.rpm",
     }
 
 
     package { 'storm':
     	ensure		=> installed,
     	provider	=> rpm,
-    	source		=> 'https://github.com/downloads/rfliam/storm-rhel-packaging/storm-0.7.4-1.el6.x86_64.rpm',
+    	#source		=> 'https://github.com/downloads/rfliam/storm-rhel-packaging/storm-0.7.4-1.el6.x86_64.rpm',
+    	source		=> 'http://dl.dropbox.com/u/25821613/storm/storm-0.7.4-1.el6.x86_64.rpm',
     	require		=> [Package['zeromq'], Package['zmq']]
     }
 
