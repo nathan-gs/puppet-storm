edccfb276fb10425ee3343260fa09038fc871700
Nathan Bijnens <nathan@nathan.gs>
2013-01-02 11:53:13 +0100

    storm: upgraded to storm-0.8.1

--- manifests/redhat.pp
+++ manifests/redhat.pp
@@ -21,7 +21,7 @@ class storm::redhat
     	ensure		=> installed,
     	provider	=> rpm,
     	#source		=> 'https://github.com/downloads/rfliam/storm-rhel-packaging/storm-0.7.4-1.el6.x86_64.rpm',
-    	source		=> 'http://dl.dropbox.com/u/25821613/storm/storm-0.7.4-1.el6.x86_64.rpm',
+    	source		=> 'https://dl.dropbox.com/u/25821613/storm/storm-0.8.1-1.el6.x86_64.rpm',
     	require		=> [Package['zeromq'], Package['jzmq']]
     }
 
